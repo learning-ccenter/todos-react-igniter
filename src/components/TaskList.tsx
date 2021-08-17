a7b6271ccae4d5cef20c5670d4fcd77372c6fb3d
Clauter Carlos <clauter.carlos@gmail.com>
2021-08-17 10:23:59 -0300

    Challenge has been implemented

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const data = {
+      id: Math.floor(Math.random() * 1000),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    newTaskTitle && setTasks([...tasks, data])
+    setNewTaskTitle('');
+    console.log(tasks);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const statusTaks = tasks.map(task => {
+      if(task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+    });
+    setTasks(statusTaks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id);
+    setTasks(removeTask);
   }
 
   return (
